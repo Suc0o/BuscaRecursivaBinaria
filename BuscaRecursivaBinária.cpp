84d69c4b8b4bdf09668b9d0f96433aca43d814d6
Vinícius Mancini Meneses de Miranda <89050874+Suc0o@users.noreply.github.com>
2023-04-14 00:00:26 -0300

    Update BuscaRecursivaBinária.cpp

--- BuscaRecursivaBinária.cpp
+++ BuscaRecursivaBinária.cpp
@@ -20,15 +20,27 @@ int busca(int lista[], int chave, int tam){
 	
 	int meio = tam / 2;
 	
-	if(meio > chave){
-		
+	if(lista[meio] > chave){
 		for(int i = 0; i < meio; i++){
-			if(lista[i] = chave){
-				return 
+			if(lista[i] == chave){
+				return i;
 			}
 		}
-		
 	}
+
+	else if(lista[meio] < chave){
+		for(int i = meio; i < tam; i++){
+			if(lista[i] == chave){
+			    return i;
+			}
+		}
+	}
+
+	else if(lista[meio] == chave){
+		return meio;
+	}
+
+    return -1;
 }
 
 int main(){
@@ -37,15 +49,27 @@ int main(){
 	int vet[] = {2, 90, 55, 20, 44, 85, 200, 10, 4, 5};
 	int tam = sizeof(vet) / sizeof(int);
 	
-	ordena(tam, vet);
+    ordena(tam, vet);
+	
+		for(int i = 0; i < tam; i++){
+		printf("%d ", vet[i]);
+	}
+	
+	printf("\n\n");
 	
 	printf("Digite o numero que deseja buscar: ");
 	scanf("%d", &num);
 	
-	busca(vet, num, tam);
+	int result = busca(vet, num, tam);
 	
-	printf("");
+	if(result){
+	printf("\nO numero %d esta no endereco %d e o %d na lista ", num, result, result + 1);
+	}
 	
+	else{
+		printf("\nO numero nao existe na lista");
+	}
+	
+	return 0;
 
 }
-
